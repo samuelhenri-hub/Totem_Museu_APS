2ff84db391d2a13d5ee5907ee3c57db5c1000784
henri <henrisamuel234@gmail.com>
2026-04-24 20:06:21 -0300

    Feat: Modelo de classe "Estatistica"

---
+++ src/Modelos/Estatistica.java
@@ -0,0 +1,11 @@
+package Modelos;
+
+public class Estatistica {
+    protected int Questao;
+    protected int indiceEscolhido;
+    protected int totalAcertos;
+
+    public int getTotalAcertos() {
+        return totalAcertos;
+    }
+}
