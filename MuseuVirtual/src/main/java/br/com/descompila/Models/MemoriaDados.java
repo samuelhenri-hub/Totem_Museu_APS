ac7aaee35401ede9102de6948581ecab541bc28d
Samuel Henri <henrisamuel234@gmail.com>
2026-05-11 17:08:55 -0300

    Arrumando
    Added a comment to clarify the purpose of the static list.

--- MuseuVirtual/src/main/java/br/com/descompila/Models/MemoriaDados.java
+++ MuseuVirtual/src/main/java/br/com/descompila/Models/MemoriaDados.java
@@ -4,6 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MemoriaDados {
-    // Lista estática que guarda todas as sessões enquanto o sistema roda
+    
     public static List<SessaoPsquisa> sessoesRealizadas = new ArrayList<>();
 }
